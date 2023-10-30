ac26d2898e061189caa444aa71ea9454251dc9b8
Aslanbey97 <107825929+Aslanbey97@users.noreply.github.com>
2023-10-30 20:25:09 +0700

    Add files via upload

---
+++ segitiga.js
@@ -0,0 +1,11 @@
+function hitungKelilingSegitiga(a, b, c) {
+    var keliling = a + b + c;
+    return keliling;
+}
+
+// Contoh penggunaan fungsi untuk menghitung keliling segitiga
+var sisiA = 4;
+var sisiB = 5;
+var sisiC = 6;
+var kelilingSegitiga = hitungKelilingSegitiga(sisiA, sisiB, sisiC);
+console.log("Keliling segitiga dengan sisi", sisiA, sisiB, "dan", sisiC, "adalah", kelilingSegitiga);

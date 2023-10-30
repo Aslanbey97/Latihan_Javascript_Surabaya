88ca0e5aed6eab69a96473879b8d6a9232be16ff
Aslanbey97 <107825929+Aslanbey97@users.noreply.github.com>
2023-10-30 20:22:43 +0700

    Add files via upload

---
+++ persegi.js
@@ -0,0 +1,9 @@
+function hitungLuasPersegi(sisi) {
+    var luas = sisi * sisi;
+    return luas;
+}
+
+// Contoh penggunaan fungsi untuk menghitung luas persegi dengan sisi 5
+var sisiPersegi = 5;
+var luasPersegi = hitungLuasPersegi(sisiPersegi);
+console.log("Luas persegi dengan sisi", sisiPersegi, "adalah", luasPersegi);
